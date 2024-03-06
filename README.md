87137458fa505493dac87eb2bf1f1ab67f590a16
Timo Otsing <timo.otsing@gmail.com>
2024-03-06 14:51:56 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# credy
-credy application
---
+++ post_application.php
@@ -0,0 +1,55 @@
+<?php
+
+$url = 'https://cv.microservices.credy.com/v1';
+$unixTimestamp = time();
+$signature = sha1($unixTimestamp . 'credy');
+
+$bio = "Passionate Junior Software Developer with 1.5 years of experience in web development using JavaScript,
+        Vue.js, and Yii2, and working with databases like MySQL and PostgreSQL. Currently a student at Kood/Jõhvi,
+        set to graduate this year. Committed to continuous learning and staying updated on industry advancements.
+        Open to new challenges andseeking opportunities to contribute to innovative projects.
+        Let's connect and explore potential collaborations!";
+
+$data = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<json:object xsi:schemaLocation="http://www.datapower.com/schemas/json jsonx.xsd"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:json="http://www.ibm.com/xmlns/prod/2009/jsonx">
+  <json:string name="first_name">Timo</json:string>
+  <json:string name="last_name">Otsing</json:string>
+  <json:string name="email">timo.otsing@gmail.com</json:string>
+  <json:string name="bio">$bio</json:string>
+  <json:array name="technologies">
+    <json:string>Yii2</json:string>
+    <json:string>PHP</json:string>
+    <json:string>Symfony</json:string>
+    <json:string>Geoserver</json:string>
+    <json:string>Openlayers</json:string>
+    <json:string>MySQL</json:string>
+    <json:string>PostgreSQL</json:string>
+    <json:string>Vue.js</json:string>
+    <json:string>Javascript</json:string>
+    <json:string>HTML</json:string>
+    <json:string>CSS</json:string>
+    <json:string>Docker</json:string>
+    <json:string>GO</json:string>
+  </json:array>
+  <json:number timestamp="timestamp">$unixTimestamp</json:number>
+  <json:string name="signature">$signature</json:string>
+  <json:string name="vcs_uri">https://github.com/timootsing/credy.git</json:string>
+</json:object>
+XML;
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: text/xml; charset=utf-8',
+        'Content-Length: ' . strlen($data))
+);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+echo $response;
